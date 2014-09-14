10eecfe7f588cfa9b5a83c1ce74a58ca60558ef5
johnsonz <957411207@qq.com>
2014-09-14 23:57:25 +0800

    Async and Stored Procedures

--- ContosoUniversity/DAL/SchoolContext.cs
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -27,6 +27,7 @@ namespace ContosoUniversity.DAL
                 .Map(t => t.MapLeftKey("CourseID")
                     .MapRightKey("InstructorID")
                     .ToTable("CourseInstructor"));
+            modelBuilder.Entity<Department>().MapToStoredProcedures();
         }
     }
 }
